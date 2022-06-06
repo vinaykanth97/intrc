bb906d1ecc0735a0e4bb76c55981c126f43d07a7
vinay_kanth97 <vinaykennedy@gmail.com>
2022-06-06 11:53:30 -0700

    Homepage Completed

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@
 if ($('.homepage').length > 0) {
     new Swiper(".hero-carousel", {
         slidesPerView: 1,
+        effect: "fade",
         pagination: {
             el: ".swiper-pagination",
             clickable: true,
@@ -13,8 +14,36 @@ if ($('.homepage').length > 0) {
             loadPrevNextAmount: 1
         }
     });
-    $('.down-arrow').on('click', function () {
-        let nextSec = $('.who-we-are').offset().top
+    new Swiper(".testimonials", {
+        slidesPerView: 3,
+        spaceBetween: 16,
+        navigation: {
+            nextEl: '.swiper-button-next-unique',
+            prevEl: '.swiper-button-prev-unique'
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    });
+    new Swiper(".client-partner-slider", {
+        slidesPerView: 3,
+        spaceBetween: 16,
+        navigation: {
+            nextEl: '.swiper-button-next-unique',
+            prevEl: '.swiper-button-prev-unique'
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    });
+    ScrollToDown('.down-arrow', '.who-we-are')
+}
+
+function ScrollToDown(element, downElement) {
+    $(element).on('click', function () {
+        let nextSec = $(downElement).offset().top - $('header').height()
         window.scrollTo({
             top: nextSec,
             behavior: 'smooth'
@@ -23,7 +52,11 @@ if ($('.homepage').length > 0) {
 }
 
 // Header scroll
-window.addEventListener('scroll', () => {
+function ScrollActiveHeader() {
     let header = document.querySelector('header')
     window.pageYOffset > 100 ? header.classList.add('active') : header.classList.remove('active')
+}
+ScrollActiveHeader()
+window.addEventListener('scroll', () => {
+    ScrollActiveHeader()
 })
