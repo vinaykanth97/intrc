25178e940b2487aba0a8af191475072ec217e3de
vinay_kanth97 <vinaykennedy@gmail.com>
2022-07-17 09:50:49 -0700

    form testing

--- src/contact_mail.php
+++ src/contact_mail.php
@@ -3,18 +3,18 @@ $mail = new PHPMailer();
 $mail->IsSMTP();
 $mail->SMTPDebug = 0;
 $mail->SMTPAuth = TRUE;
-$mail->SMTPSecure = "tls";
-$mail->Port = 587;
-$mail->Username = "Your SMTP UserName";
-$mail->Password = "Your SMTP Password";
-$mail->Host = "smtpout.secureserver.net";
+$mail->SMTPSecure = "SSL";
+$mail->Port = 465;
+$mail->Username = "vinaykennedy@gmail.com";
+$mail->Password = "Welc0me2$";
+$mail->Host = "smtp.gmail.com";
 $mail->Mailer = "smtp";
 $mail->SetFrom($_POST["userEmail"], $_POST["userName"]);
 $mail->AddReplyTo($_POST["userEmail"], $_POST["userName"]);
-$mail->AddAddress("recipient address");
-$mail->Subject = $_POST["subject"];
+$mail->AddAddress("kanthvinay7@gmail.com");
+$mail->Subject = $_POST["INTRC Contact form Lead"];
 $mail->WordWrap = 80;
-$mail->MsgHTML($_POST["content"]);
+$mail->MsgHTML($_POST["fname"]);
 
 
 
